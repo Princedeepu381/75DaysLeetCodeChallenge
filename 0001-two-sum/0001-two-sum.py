b38316de1616e8ebd283d2afb499d4b5250e5ffa
Princedeepu381 <130448527+Princedeepu381@users.noreply.github.com>
2026-03-12 20:31:26 +0530

    Time: 0 ms (100%), Space: 20.3 MB (65.5%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,6 +1,6 @@
 class Solution():
     def twoSum(self,nums:list[int],target:int)->list[int]:
-        num_map={}
+        num_map={}# stores seen number and indices 
         for i,num in enumerate (nums):
             complement=target-num
             if complement in num_map:
